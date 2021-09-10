a4e4859810e816efd00507344d1751c9765d1aa3
Parth Gupta <82574933+parth-gpt10@users.noreply.github.com>
2021-09-10 21:49:39 +0530

    Add files via upload

---
+++ Day16/16-1.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+turt = Turtle()
+print(turt)
+turt.shape("turtle")
+turt.color("green")
+turt.forward(100)
+
+screen = Screen()
+print(screen.canvheight)
+screen.exitonclick()
