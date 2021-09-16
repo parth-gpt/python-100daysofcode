2cb24e70aae9a696e2f50b63e4524357ae0ca914
Parth Gupta <82574933+parth-gpt10@users.noreply.github.com>
2021-09-16 17:42:50 +0530

    Add files via upload

---
+++ Day19/Project-2/main.py
@@ -0,0 +1,42 @@
+import random
+from turtle import Turtle, Screen
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Place your bet.", prompt="Which turtle will win enter the color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_pos = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle("turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_pos[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        turtle.forward(random.randint(0, 10))
+        if turtle.xcor() > 230:
+            is_race_on = False
+            win_color = turtle.pencolor()
+            if win_color == user_bet:
+                print(f"You've won! The {win_color} turtle is the winner.")
+            else:
+                print(f"You've lost! The {win_color} turtle is the winner.")
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
